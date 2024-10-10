64f1cff56526c442c5a8e01434508b1f7d6d4170
pakizahsaqib <pakizahsaqib@gmail.com>
2024-10-10 09:52:54 +0500

    Signup added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <signup.cpp>
 using namespace std;
 
 int main(){
     cout << "Hello World"<< endl;
     cout << "Second line of code"
+    signup()
     return 0;
 }
---
+++ signup.cpp
@@ -0,0 +1,4 @@
+int signup(){
+    cout << "This is signup code"<< endl;
+    return 0;
+}
